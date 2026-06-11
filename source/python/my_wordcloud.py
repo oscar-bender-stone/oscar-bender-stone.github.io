fb908faacb5263c9b6dc2c0863039f9600abac55
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-06-11 12:49:07 -0600

    feat: improve wordcloud palette

--- source/python/my_wordcloud.py
+++ source/python/my_wordcloud.py
@@ -2,9 +2,11 @@
 # SPDX-License-Identifier: MIT
 
 # TODO: adjust default font
+# TODO: adjust palette as needed
 
 import json
 
+import matplotlib.colors
 from wordcloud import WordCloud
 
 # WARNING: relative path is based on
@@ -12,5 +14,25 @@ from wordcloud import WordCloud
 with open("./assets/interests.json") as file:
     word_counts = json.load(file)
 
-wordcloud = WordCloud(background_color="white").generate_from_frequencies(word_counts)
+# Inspired by this theme:
+# https://duckduckgo.com/?q=doom+one+github&ia=web
+palette = {
+    "red": "#ff6c6b",
+    "orange": "#da8548",
+    "yellow": "#ecbe7b",
+    "green": "#98be65",
+    "blue": "#51afef",
+    "sky": "#d1e0ff",
+    "orchid": "#be95ff",
+    "purple": "#A2A1FF",
+    "magenta": "#e492ff",
+}
+
+colormap = matplotlib.colors.LinearSegmentedColormap.from_list(
+    "doom_one", list(palette.values())
+)
+
+wordcloud = WordCloud(
+    background_color="white", colormap=colormap
+).generate_from_frequencies(word_counts)
 wordcloud.to_file("./assets/images/interests_cloud.png")
