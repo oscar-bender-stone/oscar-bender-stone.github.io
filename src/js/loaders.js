aef9f7d2c59ffbde3a18b5bb5d093fd48b03e5eb
Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
2026-08-24 14:49:21 -0500

    chore: remove old js folder

--- src/js/loaders.js
+++
@@ -1,4 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
-// SPDX-License-Identifier: MIT
-
-import { CONFIG } from "./config.js";
--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Oscar Bender-Stone <oscar-bender-stone@protonmail.com>
-// SPDX-License-Identifier: MIT
-
-import { loadMainInfo } from "./loaders.js";
-
-loadMainInfo();
